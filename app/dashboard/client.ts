a70ce47094a610a96df2a7e6ae51d77afa64c4b1
jweboy <jweboy0630@gmail.com>
2025-03-23 02:13:36 +0800

    feat: 增加调试日志

--- app/dashboard/client.ts
+++ app/dashboard/client.ts
@@ -24,6 +24,8 @@ const httpLink = createHttpLink({
 const authLink = setContext(async (_, { headers }) => {
   // const token = (await supabase.auth.getSession()).data.session?.access_token;
 
+  console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
   return {
     headers: {
       ...headers,
--- app/dashboard/supabase.ts
+++ app/dashboard/supabase.ts
@@ -1,5 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(
+  "supabase",
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
